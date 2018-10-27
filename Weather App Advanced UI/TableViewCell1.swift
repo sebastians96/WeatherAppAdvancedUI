bcd435e22b924429d7e859e3744586a0b1a85713
Iza <iza@MacBook-Air-Iza.local>
2018-10-27 06:27:54 +0200

    50% work done

---
+++ Weather App Advanced UI/TableViewCell1.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell1.swift
+//  Weather App Advanced UI
+//
+//  Created by Iza on 27.10.2018.
+//  Copyright © 2018 Sebastian S. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Weather App Advanced UI/WeatherFetcher.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherFetcher.swift
+//  Weather App Advanced UI
+//
+//  Created by Iza on 27.10.2018.
+//  Copyright © 2018 Sebastian S. All rights reserved.
+//
+
+import Foundation
